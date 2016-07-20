653a09e829c1a557882ab3d413bad262b40af73a
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2016-07-20 10:14:33 +0100

    Updated config file and removed all clientids, server tokens and secret keys

--- config/config.js
+++ config/config.js
@@ -1,20 +1,10 @@
 var development = {
-    db: {
-        url                 : 'mongodb://localhost/executer',
-    },
-    firebase: {
-        rootRefUrl          : "https://project-4946325313836123278.firebaseio.com/",
-        secretKey           : "mJ7NQ9eTXCowtmSzAAWqDFAF9n9kiufKdVxD09hQ"
-    },
     calendar: {
-        clientId            : "453264479059-fc56k30fdhl07leahq5n489ct7ifk7md.apps.googleusercontent.com",
-        clientSecret        : "XA-7mkghxVKC-iXkCR9VnAbe",
         authorize_url       : "https://www.googleapis.com/auth/calendar.readonly",
         events_url          : "https://www.googleapis.com/calendar/v3/calendars/primary/events",
         callBackURL         : "http://localhost:5555/calendar/callback"
     },
     uber: {
-        clientId            : "rr2NzvHi69QJalUHz0ImU1KidoE1KGc5",
         access_token_url    : "https://login.uber.com/oauth/token",
         authorize_url       : "https://login.uber.com/oauth/authorize",
         base_url            : "https://login.uber.com/",
@@ -23,27 +13,17 @@ var development = {
         base_uber_url_v1_1  : "https://api.uber.com/v1.1/",
         profile_url         : "https://api.uber.com/v1/me",
         sandbox_base_url    : "https://sandbox-api.uber.com/v1/",
-        redirect_url        : "http://localhost:5555/uber/callback",
-        secretKey           : "aw-quoDm9pKUvQL9NrgdwlUcegrj_JOJpRSVtUj4",
-        server_token        : "WaxCkdTlaVFmB9Vf76q_buaTGqVad5ODrYX5S5h2"
+        redirect_url        : "http://localhost:5555/uber/callback"
     }
 };
 
 var production = {
-    db: {
-        url                 : ''
-    },
-    firebase: {
-        rootRefUrl          : "https://project-4946325313836123278.firebaseio.com/",
-        secretKey           : "mJ7NQ9eTXCowtmSzAAWqDFAF9n9kiufKdVxD09hQ"
-    },
     calendar: {
-        clientId            : "453264479059-fc56k30fdhl07leahq5n489ct7ifk7md.apps.googleusercontent.com",
-        clientSecret        : "XA-7mkghxVKC-iXkCR9VnAbe",
+        authorize_url       : "https://www.googleapis.com/auth/calendar.readonly",
+        events_url          : "https://www.googleapis.com/calendar/v3/calendars/primary/events",
         callBackURL         : "https://andelahack.herokuapp.com/calendar/callback"
     },
     uber: {
-        clientId            : "rr2NzvHi69QJalUHz0ImU1KidoE1KGc5",
         access_token_url    : "https://login.uber.com/oauth/token",
         authorize_url       : "https://login.uber.com/oauth/authorize",
         base_url            : "https://login.uber.com/",
@@ -52,39 +32,45 @@ var production = {
         base_uber_url_v1_1  : "https://api.uber.com/v1.1/",
         profile_url         : "https://api.uber.com/v1/me",
         sandbox_base_url    : "https://sandbox-api.uber.com/v1/",
-        redirect_url        : "https://andelahack.herokuapp.com/uber/callback",
-        secretKey           : "aw-quoDm9pKUvQL9NrgdwlUcegrj_JOJpRSVtUj4",
-        server_token        : "WaxCkdTlaVFmB9Vf76q_buaTGqVad5ODrYX5S5h2"
+        redirect_url        : "https://andelahack.herokuapp.com/uber/callback"
     }
 };
 
 var staging = {
-    db: {
-        url                 : ''
-    },
-    firebase: {
-        rootRefUrl          : "https://project-4946325313836123278.firebaseio.com/",
-        secretKey           : "mJ7NQ9eTXCowtmSzAAWqDFAF9n9kiufKdVxD09hQ"
-    },
     calendar: {
-        clientId            : "453264479059-fc56k30fdhl07leahq5n489ct7ifk7md.apps.googleusercontent.com",
-        clientSecret        : "XA-7mkghxVKC-iXkCR9VnAbe",
+        authorize_url       : "https://www.googleapis.com/auth/calendar.readonly",
+        events_url          : "https://www.googleapis.com/calendar/v3/calendars/primary/events",
         callBackURL         : "http://localhost:5555/calendar/callback"
+    },
+    uber: {
+        access_token_url    : "https://login.uber.com/oauth/token",
+        authorize_url       : "https://login.uber.com/oauth/authorize",
+        base_url            : "https://login.uber.com/",
+        scopes              : ["profile", "history_lite", "request"],
+        base_uber_url       : "https://api.uber.com/v1/",
+        base_uber_url_v1_1  : "https://api.uber.com/v1.1/",
+        profile_url         : "https://api.uber.com/v1/me",
+        sandbox_base_url    : "https://sandbox-api.uber.com/v1/",
+        redirect_url        : "https://andelahack.herokuapp.com/uber/callback"
     }
 };
 
 var test = {
-    db: {
-        url                 : ''
-    },
-    firebase: {
-        rootRefUrl          : "https://project-4946325313836123278.firebaseio.com/",
-        secretKey           : "mJ7NQ9eTXCowtmSzAAWqDFAF9n9kiufKdVxD09hQ"
-    },
     calendar: {
-        clientId            : "453264479059-fc56k30fdhl07leahq5n489ct7ifk7md.apps.googleusercontent.com",
-        clientSecret        : "XA-7mkghxVKC-iXkCR9VnAbe",
+        authorize_url       : "https://www.googleapis.com/auth/calendar.readonly",
+        events_url          : "https://www.googleapis.com/calendar/v3/calendars/primary/events",
         callBackURL         : "http://localhost:5555/calendar/callback"
+    },
+    uber: {
+        access_token_url    : "https://login.uber.com/oauth/token",
+        authorize_url       : "https://login.uber.com/oauth/authorize",
+        base_url            : "https://login.uber.com/",
+        scopes              : ["profile", "history_lite", "request"],
+        base_uber_url       : "https://api.uber.com/v1/",
+        base_uber_url_v1_1  : "https://api.uber.com/v1.1/",
+        profile_url         : "https://api.uber.com/v1/me",
+        sandbox_base_url    : "https://sandbox-api.uber.com/v1/",
+        redirect_url        : "https://andelahack.herokuapp.com/uber/callback"
     }
 };
 
